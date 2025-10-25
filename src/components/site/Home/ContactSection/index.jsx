a189d24fd7ecb84f96266ec2a3f02802f74f0f08
VDadashov <vusald79@gmail.com>
2025-10-25 13:43:53 +0400

    map size updated

--- src/components/site/Home/ContactSection/index.jsx
+++ src/components/site/Home/ContactSection/index.jsx
@@ -54,6 +54,8 @@ const ContactSection = () => {
           <WrapperBox>
             <MapWrapper>
             <iframe
+              width="100%"
+              height="100%"
               title="HotelShop Location"
               src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3037.776955505772!2d49.901317576010925!3d40.41379167144008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2saz!4v1761383084232!5m2!1sen!2saz"
               allowFullScreen
