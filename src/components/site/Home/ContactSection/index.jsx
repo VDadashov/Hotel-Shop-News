6e80ebe6d3d62ae52563c384def954d35b432bb3
VDadashov <vusald79@gmail.com>
2025-10-25 13:39:48 +0400

    home contact section update

--- src/components/site/Home/ContactSection/index.jsx
+++ src/components/site/Home/ContactSection/index.jsx
@@ -54,14 +54,12 @@ const ContactSection = () => {
           <WrapperBox>
             <MapWrapper>
             <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.9945528856555!2d49.82821567636126!3d40.377194158390344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307d69dfc5844d%3A0x93069d13e2f4ae43!2sHotelstore!5e0!3m2!1sen!2saz!4v1713355404019!5m2!1sen!2saz"
-            width="100%"
-            height="250"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
+              title="HotelShop Location"
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3037.776955505772!2d49.901317576010925!3d40.41379167144008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2saz!4v1761383084232!5m2!1sen!2saz"
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
 
             </MapWrapper>
           </WrapperBox>
